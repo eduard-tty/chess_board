dd1ff8e1b9c0b6849dbd3ed74d7152eae2419a00
Eduard <elohmann@xs4all.nl>
2011-06-04 11:07:08 +0200

    Adedded dummyargument to init, now it starts

--- src/chessboard_server.erl
+++ src/chessboard_server.erl
@@ -1,5 +1,5 @@
 %%%---------------------------------------------------------------------
-%%% @author Eduard Lohmann <eduard@tty.nl> 
+%%% @author Eduard Lohmann <eduard@tty.nl>
 %%% @copyright 2011 Eduard Lohmann
 %%% @doc Chess board utilitties. Excersize software.
 %%% @end
@@ -14,27 +14,27 @@
 -behaviour(gen_server).
 
 -export([ % chessboard functions
-	   setup/0   
-	 , move/2
-	 , display/1    
-	 , test/0
+       setup/0
+     , move/2
+     , display/1
+     , test/0
  ]).
 
 -export([ % gen_server interface
-	    init/1
-	  , handle_call/3
-	  , handle_cast/2
-	  , handle_info/2
-	  , terminate/2
-	  , code_change/3
+        init/1
+      , handle_call/3
+      , handle_cast/2
+      , handle_info/2
+      , terminate/2
+      , code_change/3
 ]).
 
--export([ % chessboard_server infterface 
-	    start_link/0
-	  , start_link/1
-	  , stop/0
-	  , move/1
-	  , show/0
+-export([ % chessboard_server infterface
+        start_link/0
+      , start_link/1
+      , stop/0
+      , move/1
+      , show/0
 ]).
 
 -import(io).
@@ -64,7 +64,7 @@ show() ->
 
 %% -------------------- gen_server API ---------------------------------
 
-init([]) ->
+init([_Port]) ->
     State = setup(),
     {ok, State}.
 
@@ -85,53 +85,53 @@ code_change(_OldVersion, State, _Extra) ->
 
 %% -------------------- chessboard API --------------------------------
 
-setup() -> [ 
-	     [ 't', 'n', 'b', 'k', 'q', 'b', 'n', 't' ], % 8
-	     [ 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p' ], % 7
-	     [ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ], % 6
-	     [ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ], % 5
-	     [ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ], % 4
-	     [ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ], % 3
-	     [ 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P' ], % 2
-	     [ 'T', 'N', 'B', 'K', 'Q', 'B', 'N', 'T' ]  % 1
-	     %  a    b    c    d    e    f    g    h
-	   ].
+setup() -> [
+         [ 't', 'n', 'b', 'k', 'q', 'b', 'n', 't' ], % 8
+         [ 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p' ], % 7
+         [ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ], % 6
+         [ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ], % 5
+         [ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ], % 4
+         [ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ], % 3
+         [ 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P' ], % 2
+         [ 'T', 'N', 'B', 'K', 'Q', 'B', 'N', 'T' ]  % 1
+         %  a    b    c    d    e    f    g    h
+       ].
 
 replace( Board, BoardRowIndex, BoardColIndex, Piece ) ->
     RowIndex = 8 - BoardRowIndex,
     ColIndex = BoardColIndex -1,
     { BeforeRows, [ OldRow | AfterRows ] } = lists:split(RowIndex, Board ),
-    { BeforePieces, [ OldPiece | AfterPieces ] } = lists:split(ColIndex, OldRow), 
+    { BeforePieces, [ OldPiece | AfterPieces ] } = lists:split(ColIndex, OldRow),
     NewRow = BeforePieces ++ [ Piece ] ++ AfterPieces,
     NewBoard = BeforeRows ++ [ NewRow ] ++ AfterRows,
     { NewBoard, OldPiece }.
 
-move(Board, [ C1, R1, $- , C2 , R2 ] ) -> 
+move(Board, [ C1, R1, $- , C2 , R2 ] ) ->
     {Board2, Piece } = replace(Board, R1 - $0, C1 -$a +1, ' '),
-    {Board3, _ }     = replace(Board2, R2 - $0, C2 -$a +1, Piece), 
+    {Board3, _ }     = replace(Board2, R2 - $0, C2 -$a +1, Piece),
     Board3.
 
 display_rows( I, [ Row | Rows ] ) ->
     io:format("    ~s~s~s~s~s~s~s~s", Row ),
     io:format("  ~B~n", [I] ),
-    display_rows(I-1, Rows);	
-display_rows( I , [] ) -> I . 
+    display_rows(I-1, Rows);
+display_rows( I , [] ) -> I .
 
-display(Board) -> 	
+display(Board) ->
     io:format("~n", []),
     display_rows(8,Board),
-    io:nl(),	
+    io:nl(),
     io:format("    abcdefgh~n~n", []).
 
 play(Board, []) -> Board;
-play(Board, [Move | Moves ]) -> 
+play(Board, [Move | Moves ]) ->
     Board2 = move(Board, Move),
     play(Board2, Moves).
 
-test() -> 
+test() ->
     B = setup(),
     B2 = play(B, [ "e2-e4", "e7-e5", "b1-c3" ] ),
     display(B2).
 
 
-	
+
